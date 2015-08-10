1803caf81215a3509b3003648802398d0df7ef38
xtofl <xtofl@github.com>
2015-08-10 13:19:14 +0200

    [impl] countdown scrpt

---
+++ block.py
@@ -0,0 +1,15 @@
+from time import sleep
+
+def timer(total_ticks):
+    ticks_left = total_ticks
+    while ticks_left != 0:
+        print("{}/{}".format(ticks_left, total_ticks))
+        sleep(1)
+        ticks_left = ticks_left - 1
+
+def buzz():
+    print("TODO: ... buzzzzz")
+
+timer(15)
+buzz()
+
